d15a480360168bdbba0c043ba123e7a8e2a039f2
Konstantin Biriukov <kostya-leon@mail.ru>
2025-01-16 15:17:00 -0600

    hm2

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,24 @@ import { createRoot } from "react-dom/client";
 const rootElement = document.getElementById("root");
 
 const reactRoot = createRoot(rootElement);
+import { restaurants} from "../materials/mock.js";
 
-// reactRoot.render(
-//   createElement(
-//     "ul",
-//     { style: { color: "red" }, className: "someClass" },
-//     [1, 2, 3, 4, 5].map((item) => createElement("li", {}, item))
-//   )
-// );
-
-// jsx
 reactRoot.render(
-  <ul style={{ color: "red" }} className='someClass'>
-    {[1, 2, 3, 4, 5].map((item) => (
-      <li key={item}>{item}</li>
-    ))}
-  </ul>
+    <div  className='someClass'>
+      {restaurants.map((restaurant) => (
+          <div>
+            <h1>Название ресторана: <span key={restaurant}>{restaurant.name}</span>
+            </h1>
+            <h3>Меню</h3>
+            <ul>
+              {restaurant.menu.map(dish => <li key={dish}>{dish.name}</li>)}
+            </ul>
+            <h3>Отзывы</h3>
+            <ul>
+              {restaurant.reviews.map(review => <li key={review}>{review.text}</li>)}
+            </ul>
+            <hr/>
+          </div>
+      ))}
+    </div>
 );
