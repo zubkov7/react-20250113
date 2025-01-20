34a9d06411814a851ca388ed3f6fb247bb371374
azubkovd <zubkov.andrey.99@mail.ru>
2025-01-20 19:30:24 +0300

    hw-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
 import { createRoot } from "react-dom/client";
+import { headphones } from "./constants/mock";
 
 const rootElement = document.getElementById("root");
 
 const reactRoot = createRoot(rootElement);
 
-// reactRoot.render(
-//   createElement(
-//     "ul",
-//     { style: { color: "red" }, className: "someClass" },
-//     [1, 2, 3, 4, 5].map((item) => createElement("li", {}, item))
-//   )
-// );
-
-// jsx
 reactRoot.render(
-  <ul style={{ color: "red" }} className='someClass'>
-    {[1, 2, 3, 4, 5].map((item) => (
-      <li key={item}>{item}</li>
+  <div>
+    <h1>Headphones</h1>
+    {headphones.map(({ name, id, reviews, codecs }) => (
+      <div key={id}>
+        <h2>{name}</h2>
+        <h3>Reviews</h3>
+        {reviews.map((text) => (
+          <span key={text}>{text}</span>
+        ))}
+        <h3>Codecs</h3>
+        {codecs.map((codec) => (
+          <span key={codec}>{codec}</span>
+        ))}
+      </div>
     ))}
-  </ul>
+  </div>
 );
